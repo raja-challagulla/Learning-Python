b09b238e0238cc6b9d6b9eb5decb758b61e7c7e8
Raja Challagulla (CS) <Raja.Challagulla@veritas.com>
2020-03-17 22:46:14 +0800

    first Simple program

---
+++ tutorials_gateway/Addition.py
@@ -0,0 +1,9 @@
+""" Addition of two numbers
+"""
+
+a = int(input("Enter a number : "))
+b = int(input("Enter 2nd number : "))
+
+print(a+b)
+print("The addition of {0} and {1} is : {2}".format(a, b, a+b))
+
