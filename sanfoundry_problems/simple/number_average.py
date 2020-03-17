6b34d9d4f19287781057d9b5aae98680fcdd9d5e
Raja Challagulla <raja.challagulla8@gmail.com>
2020-03-18 07:21:20 +0800

    Version 1.0

---
+++ sanfoundry_problems/simple/number_average.py
@@ -0,0 +1,14 @@
+""" A program to check average of numbers
+"""
+
+# Read total numbers count from the user
+
+n = int(input("Enter the count of numbers of to calculate Average : "))
+assert n > 0
+number_list = []
+for i in range(n):
+    element = int(input(f"Enter the {i} number in the list: "))
+    number_list.append(element)
+
+num_avg = sum(number_list)/n
+print("the average is :", num_avg)
